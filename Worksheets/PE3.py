da1008228a2fd22d1eb7cbfbbe03708f0f94cd62
Pan Sui Wang <112232636+tst-pandit-pan@users.noreply.github.com>
2023-10-22 14:38:59 +0000

    Updated

--- Worksheets/PE3.py
+++ Worksheets/PE3.py
@@ -4,10 +4,10 @@ for i in range(20):
     year = i + 1
     print("{:>3}".format(year), end = "")
     #print the year
-    print("{:>2}".format(":"), end = "")
+    print("{:>3}".format(":"), end = "")
     #print the colon
-    for j in range(3,11):
+    for j in range(3,10):
         amount = 100 * (1 + j / 100) ** year
         #calculate the amount get after interest given, replace 100 with input if needed
-        print("{:^7.2f}".format(amount), end = "")
+        print("{:^8.2f}".format(amount), end = "")
     print()
