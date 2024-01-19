2372c6512e4dd7d46c6f1680104bdb5c51829a4f
Pan Sui Wang <112232636+tst-pandit-pan@users.noreply.github.com>
2024-01-19 04:25:10 +0000

    Updated

---
+++ PE/PE8.py
@@ -0,0 +1,39 @@
+import random
+# Initialization
+n = [0, 0, 0, 0, 0, 0, 0]
+sn = [0, 0, 0, 0, 0, 0]
+
+# Generating 7 non-repeating numbers between 1 and 49
+n[0] = int(random.random() * 49) + 1
+exist = False
+for i in range(1,?):
+    while exist:
+      n[i] = ?(random.random() * ?) + 1
+      exist = ?
+      j = 0
+      while (j <= ?) ? not exist:
+          if ? :   # check the current generated num exists in the previously generated num of the list
+              exist = ?
+          ?
+    ?									# the most challenging one
+
+# Sort the numbers in ascending order
+for i in range(?):
+    min = n[0]
+    pmin = 0
+    for j in range(1,?):
+        if ? :
+            min = ?
+            pmin = ?
+    sn[i] = ?     # copy the minimum value of round i to the new array sn[]
+    n[pmin] = ?   # assign a value big enough so that the num copied to sn[] will not be minimum anymore
+
+# Displaying the numbers generated
+print("The six drawn numbers are ", end = "")
+for i in range(?):
+    print(?, end = "")
+    if i != ?:
+        print(", ", end = "")
+    else:
+        print(?)
+        print("The extra-number is " + int(n[?]) + ".")
