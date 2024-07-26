1e2c96c6d3a8ffdd99e46f97cde96c6f93174cc9
Pan Sui Wang <112232636+tst-pandit-pan@users.noreply.github.com>
2024-07-26 04:31:50 +0000

    Updated HKOI

---
+++ HKOI/D802.py
@@ -0,0 +1,26 @@
+N, A = input().split()
+N = int(N)
+A = int(A)
+numbers = []
+count = 0
+numbers = input().split()
+if A == 0:
+    for i in range(len(numbers)):
+        for j in range(len(numbers) - 1):
+            if numbers[j] > numbers[j + 1]:
+                tmp = numbers[j]
+                numbers[j] = numbers[j + 1]
+                numbers[j + 1] = tmp
+                count = count + 1
+elif A == 1:
+    for i in range(len(numbers)):
+        for j in range(len(numbers) - 1):
+            if numbers[j] < numbers[j + 1]:
+                tmp = numbers[j]
+                numbers[j] = numbers[j + 1]
+                numbers[j + 1] = tmp
+                count = count + 1
+print(count)
+for i in range(len(numbers)):
+    print(numbers[i], end=' ')
+print()
